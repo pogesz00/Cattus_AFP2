4eaeec911259e353abbb2bcf5d05c367dd01b9e8
Kacsuu <pal.joci2003@gmail.com>
2024-04-15 18:25:09 +0200

    created laravel
    generated the laravel project and created migrations to create the database

---
+++ Cattus/database/migrations/2024_04_15_160809_create_positions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cat_id')->references('id')->on('cats');
+            $table->timestamp('time')->useCurrent();
+            $table->float('x');
+            $table->float('y');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('positions');
+    }
+};
--- README.md
+++ README.md
@@ -6,3 +6,8 @@ HOW TO START BACKEND
 3. cd cattus
 4. nodemon start
 5. localhost:3100 in the browser
+
+HOW TO START LARAVEL
+1. open terminal
+2. cd Cattus
+3. php artisan serve
